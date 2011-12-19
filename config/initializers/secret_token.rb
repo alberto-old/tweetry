c362e4c4de364360e6d5cea4d013aacba7eb39c4
Alberto Villa <alberto@railsmoon.(none)>
2011-12-19 21:17:30 +0100

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetry::Application.config.secret_token = '9cc439074b5528b4a86bf5f3fcb27cf6023660ffd17852d6dff0d11330ea4fd34d7a82d9e23b0c01d37acab165e7cd651efb815981852853f0803687fa8ff326'
